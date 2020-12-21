a522f7b24964bb74b2b666e497726a7adcc9e694
crane0221 <75875765+crane0221@users.noreply.github.com>
2020-12-21 19:39:01 +0800

    Add files via upload

---
+++ sql新增/sql新增/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sql新增
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection con = cn1.cnn)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText =
+                    $"Insert Into Account (AccountID,Name) Values (N'36',N'Ther')";
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
